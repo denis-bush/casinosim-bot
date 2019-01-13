e46e66c25cafa920d48d70b5b0c88b81f0c1d690
denis-bush <denis.bush98@gmail.com>
2019-01-14 01:56:26 +0300

    again

--- casinobot.py
+++ casinobot.py
@@ -1,26 +1,6 @@
-import requests
+import telebot
+import parser
 
-url = "https://api.telegram.org/bot631046420:AAHgOJwxSO8g1-hN9boIJYOC-nPEWKN-mDc"
-
-
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
-
-
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
-
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
+#main variables
+TOKEN = "631046420:AAHgOJwxSO8g1-hN9boIJYOC-nPEWKN-mDc"
+bot = telebot.TeleBot(TOKEN)
