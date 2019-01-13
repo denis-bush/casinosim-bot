c8a2f1fddba82d7206f3b929df01e667054c0bda
denis-bush <denis.bush98@gmail.com>
2019-01-13 19:05:34 +0300

    Initial commit

---
+++ telegram-casino-bot-master/games/__init__.py
@@ -0,0 +1,35 @@
+from games.oneshot import Oneshot
+from games.bulldozer import Bulldozer
+from games.even import Even
+from games.odd import Odd
+from games.insurance import Insurance
+from games.double import Double
+from games.seven import Seven
+from games.triple import Triple
+from games.increasing import Increasing
+from games.decreasing import Decreasing
+from games.devil import Devil
+from games.extremes import Extremes
+from games.guess import Guess
+from games.first import First
+from games.middle import Middle
+from games.last import Last
+
+games = [
+    Oneshot,
+    Even,
+    Odd,
+    First,
+    Middle,
+    Last,
+    Insurance,
+    Bulldozer,
+    Double,
+    Extremes,
+    Seven,
+    Triple,
+    Increasing,
+    Decreasing,
+    Devil,
+    Guess,
+]
---
+++ telegram-casino-bot-master/round.py
@@ -0,0 +1,65 @@
+import hashlib
+import os
+import random
+
+CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+
+class UnacceptableBetError(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+def generate_seed():
+    return ''.join(random.choice(CHARS) for i in range(15))
+
+
+class Round(object):
+
+    def __init__(self, payout_limit=None):
+        self.seed = generate_seed()
+        self.proof = hashlib.md5(self.seed.encode('utf-8')).hexdigest()[0:8]
+        random.seed(self.seed)
+        self.random_state = random.getstate()
+        self.bets = []
+        self.to_confirm_bets = []
+        self.draws = []
+        self.payout_limit = payout_limit
+        self.status = 'open'
+
+    @property
+    def total_round_payout(self):
+        """Returns the max possible payout at the end of this round"""
+        return sum(b.max_payout for b in self.bets)
+
+    def add_bet(self, bet):
+        if self.total_round_payout + bet.max_payout > self.payout_limit:
+            raise UnacceptableBetError(
+                'Non posso accettare questa puntata, mi farebbe superare il '
+                'limite di payout (possibile vincita) di questo round.\n'
+                'Payout rimanente: <b>{}</b>'.format(
+                    self.payout_limit - self.total_round_payout))
+        self.bets.append(bet)
+        self.to_confirm_bets.append(bet)
+
+    def cancel_last_bet(self, player):
+        bets = list(b for b in self.bets if b.player.name == player.name)
+        if len(bets) == 0:
+            return None
+        else:
+            self.bets.remove(bets[-1])
+            return bets[-1]
+
+    def go(self):
+        """Make alle the necessary draws"""
+        i = 0  # failsafe to avoid infinite loop
+        while any(b.must_draw(self.draws) for b in self.bets) and i < 100:
+            random.setstate(self.random_state)
+            self.draws.append(random.randint(1, 6))
+            self.random_state = random.getstate()
+            i += 1
+        return self.draws
