dcae5f032ec0c6f96169f1e31f77675f8f4fe0a0
denis-bush <denis.bush98@gmail.com>
2019-01-14 02:14:09 +0300

    yeah

--- casinobot.py
+++ casinobot.py
@@ -4,3 +4,8 @@ import parser
 #main variables
 TOKEN = "631046420:AAHgOJwxSO8g1-hN9boIJYOC-nPEWKN-mDc"
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+bot.polling()
