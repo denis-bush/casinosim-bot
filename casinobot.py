e13532f4b553f154ffc16b971fc9da51bb24f967
denis-bush <denis.bush98@gmail.com>
2019-01-14 02:22:09 +0300

    Update casinobot.py

--- casinobot.py
+++ casinobot.py
@@ -7,5 +7,16 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, ты сможешь сыграть со мной в казино')
 bot.polling()
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == "привет":
+        bot.send_message(chat_id, 'Привет, я бот - симулятор казино.')
+    elif text == "сыграем?":
+        bot.send_message(chat_id, 'Мы ещё закрыты, пожалуйста, приходите позже')
+    else:
+        bot.send_message(chat_id, 'Говори понятнее')
