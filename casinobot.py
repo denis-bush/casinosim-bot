a390ca09e3438e2387254f6a0fb829cd065f6bab
denis-bush <denis.bush98@gmail.com>
2019-01-14 00:05:20 +0300

    Update casinobot.py

--- casinobot.py
+++ casinobot.py
@@ -33,42 +33,42 @@ class BotHandler:
 
         return last_update
 		
-    greet_bot = BotHandler(token)  
-    greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-    now = datetime.datetime.now()
+greet_bot = BotHandler(token)  
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
+now = datetime.datetime.now()
 
 
-    def main():  
-        new_offset = None
-        today = now.day
-        hour = now.hour
+def main():  
+    new_offset = None
+    today = now.day
+    hour = now.hour
 
-        while True:
-            greet_bot.get_updates(new_offset)
+    while True:
+        greet_bot.get_updates(new_offset)
 
-            last_update = greet_bot.get_last_update()
+        last_update = greet_bot.get_last_update()
 
-            last_update_id = last_update['update_id']
-            last_chat_text = last_update['message']['text']
-            last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
 
-            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-                today += 1
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
 
-            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-                today += 1
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
 
-            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-                today += 1
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
 
-            new_offset = last_update_id + 1
+        new_offset = last_update_id + 1
 
-        if __name__ == '__main__':  
-            try:
-                main()
-            except KeyboardInterrupt:
-                exit()
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
