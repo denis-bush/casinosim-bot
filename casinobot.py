7503eb8481f232569d0d369feb0135ccf63507e3
denis-bush <denis.bush98@gmail.com>
2019-01-14 12:50:13 +0300

    Update casinobot.py

--- casinobot.py
+++ casinobot.py
@@ -1,6 +1,5 @@
 import telebot
 import bs4
-from dice import startGame
 
 #Токен
 TOKEN = "631046420:AAHgOJwxSO8g1-hN9boIJYOC-nPEWKN-mDc"
@@ -18,12 +17,14 @@ def start_handler(message):
         msg = bot.send_message(chat_id, 'Во что сыграем? 1 - кости, 2 - слот-машина')
         bot.register_next_step_handler(msg, askGame) #Выбираем игру
         isRunning = True
+        
 def askGame(message):
+    from dice import startGame
     chat_id = message.chat.id
     text = message.text
     if text == "1":
         msg = bot.send_message(chat_id, 'Добро пожаловать в игру "Кости"! 🎲')
-        bot.register_next_step_handler(msg, dice.startGame)
+        bot.register_next_step_handler(msg, startGame)
     elif text == "2":
         msg = bot.send_message(chat_id, 'Данная функция всё ещё находится в разработке')
         bot.register_next_step_handler(msg, askGame) 
