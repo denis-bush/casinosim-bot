a460c6cc3ad9da6421765d69cfd41bdd1c259b63
denis-bush <denis.bush98@gmail.com>
2019-01-14 12:17:36 +0300

    Update casinobot.py

--- casinobot.py
+++ casinobot.py
@@ -1,5 +1,6 @@
 import telebot
 import bs4
+import parser
 import dice
 
 #Токен
@@ -18,7 +19,6 @@ def start_handler(message):
         msg = bot.send_message(chat_id, 'Во что сыграем? 1 - кости, 2 - слот-машина')
         bot.register_next_step_handler(msg, askGame) #Выбираем игру
         isRunning = True
-
 def askGame(message):
     chat_id = message.chat.id
     text = message.text
@@ -29,23 +29,3 @@ def askGame(message):
         msg = bot.send_message(chat_id, 'Данная функция всё ещё находится в разработке')
         bot.register_next_step_handler(msg, askGame)
         return
-    else:
-        msg = bot.send_message(chat_id, 'Неверная команда, попробуйте ещё раз')	
-        bot.register_next_step_handler(msg, askGame)
-        return
-
-@bot.message_handler(content_types=['text'])
-def text_handler(message):
-    text = message.text.lower()
-    chat_id = message.chat.id
-    if text == "привет":
-        bot.send_message(chat_id, 'Привет, я бот - симулятор казино.')
-    elif text == "сыграем?":
-        bot.send_message(chat_id, 'Мы ещё закрыты, пожалуйста, приходите позже')
-    elif text == "стоп":
-        msg = bot.send_message(chat_id, 'Возвращаюсь в главное меню')
-        bot.register_next_step_handler(msg, start_handler)
-    else:
-        bot.send_message(chat_id, 'Я тебя не понимаю')
-		
-bot.polling(none_stop=True)
